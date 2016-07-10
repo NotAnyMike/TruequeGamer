9468d63aac92acccc7e867289c9a8e70b792e13f
Mike Woodcock <MikeW@MacBook-Air-de-Mike.local>
2016-07-09 23:35:24 -0500

    Adding single chat and list of messages, there are no js functions for this part

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
+		livereload = require('gulp-livereload'),
     nib = require('nib');
 
 gulp.task('stylus-dev', function(){
 	gulp.src('web/stylus/app.styl')
 	.pipe(stylus({use : nib()}))
-	.pipe(gulp.dest('web/'));
+	.pipe(gulp.dest('web/'))
+	.pipe(livereload({start: true}));
 });
 
 gulp.task('w-stylus', function(){
+	livereload.listen();
 	gulp.watch(['!web/stylus/app.styl','web/stylus/*.styl'], ['stylus-dev']);
 });
--- web/js/index.js
+++ web/js/index.js
@@ -1,4 +1,14 @@
 var consoleButtons = document.querySelectorAll('.consoleCheckbox, .extraFilterButton');
+var searchFieldList = document.getElementsByClassName('searchFieldContainer')[0].getElementsByTagName('ul')[0];
+var searchField = document.getElementsByClassName('searchFieldContainer')[0].getElementsByTagName('input')[0];
+var loginLink = document.getElementsByClassName('login')[0];
+var profileLink = document.getElementsByClassName('profileContainer')[0];
+var chatBubble = document.getElementsByClassName('chatBubble')[0];
+
+function profileFunction(){
+	loginLink.classList.toggle('hidden');
+	profileLink.classList.toggle('hidden');
+}
 
 for(var i = 0; i < consoleButtons.length; i++){
 	consoleButtons[i].onclick = function(e) {
@@ -9,3 +19,26 @@ for(var i = 0; i < consoleButtons.length; i++){
 		element.classList.toggle('checked');
 	}
 };
+
+
+searchField.onfocus = function(e){
+	searchFieldList.classList.remove('hidden');
+}
+
+searchField.onblur = function(e){
+	searchFieldList.classList.add('hidden');
+}
+
+loginLink.onclick = function(e){
+	
+	profileFunction();
+	chatBubble.classList.remove('hidden');
+
+}
+
+profileLink.onclick = function(){
+	
+	profileFunction();
+	chatBubble.classList.add('hidden');
+
+}

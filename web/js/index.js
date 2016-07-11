8907aed6864bc269b3d8b6fd8a08027595616be8
Mike Woodcock <MikeW@MacBook-Air-de-Mike.local>
2016-07-11 00:38:30 -0500

    Adding hover states to anchor links

--- web/js/index.js
+++ web/js/index.js
@@ -4,7 +4,12 @@ var searchField = document.getElementsByClassName('searchFieldContainer')[0].get
 var loginLink = document.getElementsByClassName('login')[0];
 var profileLink = document.getElementsByClassName('profileContainer')[0];
 var chatBubble = document.getElementsByClassName('chatBubble')[0];
+var chat = document.getElementById('chat');
+var chatExit = chat.getElementsByClassName('closeButton')[0];
+var chatListItems = chat.getElementsByTagName('li');
 var chatInputDiv = document.getElementById('chatInputDiv');
+var singleChat = document.getElementById('singleChat');
+var singleChatExit = singleChat.getElementsByClassName('closeButton')[0];
 
 function profileFunction(){
 	loginLink.classList.toggle('hidden');
@@ -41,6 +46,7 @@ profileLink.onclick = function(){
 	
 	profileFunction();
 	chatBubble.classList.add('hidden');
+	hideChat();
 
 }
 
@@ -48,4 +54,47 @@ chatInputDiv.onpaste = function(e) {
 	e.preventDefault();
 	var text = e.clipboardData.getData("text/plain");
 	document.execCommand("insertHTML", false, text);
+}
+
+singleChatExit.onclick = function(){
+	hideSingleChat();
+}
+
+for(var i = 0; i < chatListItems.length; i++){
+	chatListItems[i].onclick = function(){
+		showSingleChat();
+	}
+}
+
+chatBubble.onclick = function(){
+	chatBubble.classList.add('hidden');
+	showChat();
+}
+
+chatExit.onclick = function(){
+	chatBubble.classList.remove('hidden');
+	hideChat();
+}
+
+function hideSingleChat(){
+	if(singleChat.classList.contains('in'))
+		singleChat.classList.add('out');
+	singleChat.classList.remove('in');
+}
+
+function showSingleChat(){
+	singleChat.classList.remove('out');
+	singleChat.classList.add('in');
+}
+
+function showChat(){
+	chat.classList.remove('out');
+	chat.classList.add('in');
+}
+
+function hideChat(){
+	if(chat.classList.contains('in'))
+		chat.classList.add('out');
+	chat.classList.remove('in');
+	hideSingleChat();
 }

50a041d480a59bfc577bea5b60ada1ef1bc59257
Mike Woodcock <MikeW@MacBook-Air-de-Mike.local>
2016-07-12 10:22:42 -0500

    Setting the enviroment to work with react, babel, browserify, jsx and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
+'use strict';
+
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
+		gulpBrowser = require('gulp-browser'),
 		livereload = require('gulp-livereload'),
     nib = require('nib');
 
+let transforms = [
+	{
+		transform: "babelify"
+	}
+]
+
 gulp.task('stylus-dev', function(){
-	gulp.src('web/stylus/app.styl')
+	gulp.src('web/layout/stylus/app.styl')
 	.pipe(stylus({use : nib()}))
-	.pipe(gulp.dest('web/'))
+	.pipe(gulp.dest('web/layout'))
 	.pipe(livereload({start: true}));
 });
 
 gulp.task('w-stylus', function(){
 	livereload.listen();
-	gulp.watch(['!web/stylus/app.styl','web/stylus/*.styl'], ['stylus-dev']);
+	gulp.watch(['!web/layout/stylus/app.styl','web/layout/stylus/*.styl'], ['stylus-dev']);
+});
+
+gulp.task('react-dev', function(){
+	gulp.src('web/react/reactFiles/main.js')
+	.pipe(gulpBrowser.browserify(transforms))
+	.pipe(gulp.dest('web/react/js/'))
+	.pipe(livereload({start: true}));
+});
+
+gulp.task('w-react', function(){
+	livereload.listen();
+	gulp.watch(['!web/react/js/*.js','web/react/reactFiles/*.js'], ['react-dev']);
 });

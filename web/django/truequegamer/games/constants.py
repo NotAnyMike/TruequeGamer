709e804c6aaf86fccafe32b6d821fa8b88862a80
Mike Woodcock <mwc1.0@hotmail.com>
2017-07-05 12:57:49 -0500

    Fixing default profile pic

--- web/django/truequegamer/games/constants.py
+++ web/django/truequegamer/games/constants.py
@@ -4,7 +4,7 @@ FB_PROFILE_PIC_PART1 = "https://graph.facebook.com/"
 FB_PROFILE_PIC_PART2 = "/picture?type=large"
 FB_PROFILE_PIC_PART2_SMALL = "/picture?type=small"
 
-LARGE_PROFILE_PIC_URL = "/img/details_profile.png"
+LARGE_PROFILE_PIC_URL = "/img/default_pic.png"
 DEFAULT_LOCATION = "Bogotá, Colombia"
 
 FB_ACCESS_TOKEN = "EAAMFSTFTluYBAJ1tKIMX26iP5XO0JoTPtxJn5e2L6KQWHSpmtl3rie4xMgXrHj88KS5SSKdSqk6YjFetgnFMFOQnO16hIAxFzSS6iHHTau6NoWp9tMNYnBc3W8OoeAfUb0MZA2y43dUoM0l6y1F6SNIxPxJsZD"

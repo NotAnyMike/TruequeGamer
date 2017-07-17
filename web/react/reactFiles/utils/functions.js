752ef4875ed4acabf090301afe804eb247a4fe81
Mike W <mwc1.0@hotmail.com>
2017-07-17 13:04:13 -0500

    Fixing #157

--- web/react/reactFiles/utils/functions.js
+++ web/react/reactFiles/utils/functions.js
@@ -25,12 +25,49 @@ const Functions = {
 	},
 
 	fetchAdvanced: function(req){
-		if(self.fetch){
+		if(false && self.fetch){
 			return fetch(req)
 		}else{
 			//use xml
-			return null
 			console.log("there is no fetch");
+			return new Promise(function(resolve,reject){
+				var xmlReq = new XMLHttpRequest();
+
+				//know what to open
+				if(typeof req === 'string') xmlReq.open('get', req);
+				else xmlReq.open(req.method, req.url);
+
+				//adding headers
+				if(req.headers){
+					for(let [key,val] of req.headers.entries()){
+						xmlReq.setRequestHeader(key,val);
+					}
+				}
+
+				xmlReq.onload = function(resp){
+					if(resp.target.status >= 200 && resp.target.status < 300){
+						resp.target.json = function(){return new Promise(function(resolve, reject){resolve(JSON.parse(resp.target.responseText))})};
+						resp.target.ok = true;
+						resolve(resp.target);
+					}else{
+						reject({
+							status: resp.target.status,
+							statusText: resp.target.statusText
+						});
+					}
+				};
+				xmlReq.onerror = function(resp){
+					reject({
+						status: resp.target.status,
+						statusText: resp.target.statusText
+					});
+				};
+
+				if(req.body) xmlReq.send(req.body)
+				else xmlReq.send();
+
+			}.bind(this));
+
 		}
 	},
 
@@ -49,6 +86,7 @@ const Functions = {
 					    }
 		    return "";
 	},
+
 	getCustomHeader: function(type, url, userData, withCredentials){
 		//data.append( "json", JSON.stringify( editing ) );
 		if(!type) type = 'get';
@@ -66,12 +104,14 @@ const Functions = {
 				},
 			};
 		}
+		var data = null;
 		if(userData != null){
-			var data = JSON.stringify( userData );
+			data = JSON.stringify( userData );
 			init.body = data;
 		}
 		init.method = type;
 		var req = new Request(url, init)
+		if(data) req.body = data; // in order to use body in the XMLHttpRequest
 		return req;
 	},
 

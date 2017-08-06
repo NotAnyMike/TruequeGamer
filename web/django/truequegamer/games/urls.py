e360f521b3652e5a16fe73a20da568fb3e9911fa
Mike W <mwc1.0@hotmail.com>
2017-08-06 18:00:47 -0500

    Adding landing only in /landing

--- web/django/truequegamer/games/urls.py
+++ web/django/truequegamer/games/urls.py
@@ -8,7 +8,7 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^img/', views.img, name='img'),
-    url(r'^.*$', views.landing, name='landing_page'),
+    url(r'^landing$', views.landing, name='landing_page'),
     url(r'^api/user/$', views.CurrentUser, name='current_user'),
     url(r'^api/profile/(?P<username>.+)/$', views.SomeUser, name='user'),
     url(r'^api/(?P<serializerType>(suggestions)|(games)|(game))/(?P<console>.+)/(?P<new>.+)/(?P<sell>.+)/(?P<string>.*)/$', views.LocalSuggestions, name='local_suggestions'),
